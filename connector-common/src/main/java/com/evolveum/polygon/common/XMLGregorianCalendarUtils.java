e387da02657745cc5c42a3fbe00e2ab56448c99b
Viliam Repan <vilo.repan@evolveum.com>
2014-06-10 17:50:41 +0200

    improved parent pom for polygon, added first util class to common.

---
+++ connector-common/src/main/java/com/evolveum/polygon/common/XMLGregorianCalendarUtils.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2010-2014 Evolveum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evolveum.polygon.common;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * @author lazyman
+ */
+public class XMLGregorianCalendarUtils {
+
+    private static DatatypeFactory df = null;
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+
+    static {
+        try {
+            df = DatatypeFactory.newInstance();
+        } catch (DatatypeConfigurationException dce) {
+            throw new IllegalStateException("Exception while obtaining Datatype Factory instance", dce);
+        }
+    }
+
+    /**
+     * Converts a java.util.Date into an instance of XMLGregorianCalendar
+     *
+     * @param date Instance of java.util.Date or a null reference
+     * @return XMLGregorianCalendar instance whose value is based upon the
+     * value in the date parameter. If the date parameter is null then
+     * this method will simply return null.
+     */
+    public static XMLGregorianCalendar asXMLGregorianCalendar(Date date) {
+        if (date == null) {
+            return null;
+        } else {
+            GregorianCalendar gc = new GregorianCalendar();
+            gc.setTimeInMillis(date.getTime());
+            return df.newXMLGregorianCalendar(gc);
+        }
+    }
+
+    /**
+     * Converts an XMLGregorianCalendar to an instance of java.util.Date
+     *
+     * @param xgc Instance of XMLGregorianCalendar or a null reference
+     * @return java.util.Date instance whose value is based upon the
+     * value in the xgc parameter. If the xgc parameter is null then
+     * this method will simply return null.
+     */
+    public static Date asDate(XMLGregorianCalendar xgc) {
+        if (xgc == null) {
+            return null;
+        } else {
+            return xgc.toGregorianCalendar().getTime();
+        }
+    }
+
+    public static String asStringDate(XMLGregorianCalendar xgc) {
+        return asStringDate(asDate(xgc));
+    }
+
+    public static String asStringDate(Date date) {
+        if (date == null) {
+            return null;
+        }
+
+        return DATE_FORMAT.format(date);
+    }
+}
