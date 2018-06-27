5cc64bca1c62eae1ff924d73e7bcab4d6104a7be
Radovan Semancik <radovan.semancik@evolveum.com>
2018-06-27 18:25:23 +0200

    More util functions

--- connector-common/src/main/java/com/evolveum/polygon/common/SchemaUtil.java
+++ connector-common/src/main/java/com/evolveum/polygon/common/SchemaUtil.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015 Evolveum
+ * Copyright (c) 2015-2018 Evolveum
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,12 @@
 package com.evolveum.polygon.common;
 
 import java.util.List;
+import java.util.Set;
 
 import org.identityconnectors.common.StringUtil;
+import org.identityconnectors.framework.common.exceptions.InvalidAttributeValueException;
 import org.identityconnectors.framework.common.objects.Attribute;
+import org.identityconnectors.framework.common.objects.AttributeDelta;
 import org.identityconnectors.framework.common.objects.AttributeInfo;
 import org.identityconnectors.framework.common.objects.ObjectClassInfo;
 
@@ -34,11 +37,11 @@ public class SchemaUtil {
 			return null;
 		}
 		if (values.size() > 1) {
-			throw new IllegalArgumentException("Attribute "+attribute.getName()+" cannot have multiple values");
+			throw new InvalidAttributeValueException("Attribute "+attribute.getName()+" cannot have multiple values");
 		}
 		Object value = values.get(0);
 		if (!expectedClass.isAssignableFrom(value.getClass())) {
-			throw new IllegalArgumentException("Expected that value of attribute "+attribute.getName()+" will be of "+expectedClass+", but it was "+value.getClass());
+			throw new InvalidAttributeValueException("Expected that value of attribute "+attribute.getName()+" will be of "+expectedClass+", but it was "+value.getClass());
 		}
 		return (T)value;
 	}
@@ -46,10 +49,10 @@ public class SchemaUtil {
 	public static String getSingleStringNonBlankValue(Attribute attribute) {
 		String value = getSingleValue(attribute, String.class);
 		if (value == null) {
-			throw new IllegalArgumentException("Attribute "+attribute.getName()+" cannot be null");
+			throw new InvalidAttributeValueException("Attribute "+attribute.getName()+" cannot be null");
 		}
 		if (StringUtil.isBlank(value)) {
-			throw new IllegalArgumentException("Attribute "+attribute.getName()+" cannot be blank");
+			throw new InvalidAttributeValueException("Attribute "+attribute.getName()+" cannot be blank");
 		}
 		return value;
 	}
@@ -58,6 +61,51 @@ public class SchemaUtil {
 		for (AttributeInfo attributeInfo: icfObjectClassInfo.getAttributeInfo()) {
 			if (attributeInfo.is(attribute.getName())) {
 				return attributeInfo;
+			}
+		}
+		return null;
+	}
+	
+	public static <T> T getSingleReplaceValue(AttributeDelta delta, Class<T> expectedClass) {
+		assertReplace(delta);
+		List<Object> values = delta.getValuesToReplace();
+		if (values == null || values.isEmpty()) {
+			return null;
+		}
+		if (values.size() > 1) {
+			throw new InvalidAttributeValueException("Attribute "+delta.getName()+" cannot have multiple values");
+		}
+		Object value = values.get(0);
+		if (!expectedClass.isAssignableFrom(value.getClass())) {
+			throw new InvalidAttributeValueException("Expected that value of attribute "+delta.getName()+" will be of "+expectedClass+", but it was "+value.getClass());
+		}
+		return (T)value;
+	}
+	
+	public static String getSingleStringNonBlankReplaceValue(AttributeDelta delta) {
+		String value = getSingleReplaceValue(delta, String.class);
+		if (value == null) {
+			throw new InvalidAttributeValueException("Attribute "+delta.getName()+" cannot be null");
+		}
+		if (StringUtil.isBlank(value)) {
+			throw new InvalidAttributeValueException("Attribute "+delta.getName()+" cannot be blank");
+		}
+		return value;
+	}
+	
+	public static void assertReplace(AttributeDelta delta) {
+		if (delta.getValuesToAdd() != null && !delta.getValuesToAdd().isEmpty()) {
+			throw new InvalidAttributeValueException("Attribute "+delta.getName()+" cannot be added");
+		}
+		if (delta.getValuesToRemove() != null && !delta.getValuesToRemove().isEmpty()) {
+			throw new InvalidAttributeValueException("Attribute "+delta.getName()+" cannot be removed");
+		}
+	}
+	
+	public static AttributeDelta findDelta(Set<AttributeDelta> deltas, String attributeName) {
+		for (AttributeDelta delta: deltas) {
+			if (delta.is(attributeName)) {
+				return delta;
 			}
 		}
 		return null;
